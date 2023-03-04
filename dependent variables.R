9f6da759219e4a69975f51b023958d54be9d308e
OscarHCECO <oscar.hernandez@posgrado.ecologia.edu.mx>
2023-03-03 21:04:00 -0600

    finishing dependen variables

--- dependent variables.R
+++ dependent variables.R
@@ -19,10 +19,12 @@ plascontree=ape::read.nexus("compatplas.tre")#consensus tree
 haecontree=ape::read.nexus("compathae.tre")#consensus tree
 leucontree=ape::read.nexus("compatleu.tre")#consensus tree
 #Load parasite presence absense matrices
-plaspresabF=read.csv("repoplasmodiumPAM")
-haepresabF=read.csv("repohaemoproteusPAM")
-leupresabF=read.csv("repoleucocytozoonPAM")
-
+plaspresabF=read.csv("repoplasmodiumPAM",row.names = 1)
+plasgeoR=plaspresabF[c(1:4)]
+haepresabF=read.csv("repohaemoproteusPAM",row.names = 1)
+haegeoR=haepresabF[c(1:4)]
+leupresabF=read.csv("repoleucocytozoonPAM",row.names = 1)
+leugeoR=leupresabF[c(1:4)]
 ##Prepare trees
 plastrr<-list()
 haetrr<-list()
@@ -38,7 +40,7 @@ for(i in 1:1000){
 # Activate cluster for foreach library
 
 my.cluster <- parallel::makeCluster(
-  8,   # number of available cores  
+  6,   # number of available cores  
   type = "PSOCK"
 )
 doParallel::registerDoParallel(cl = my.cluster)
@@ -56,8 +58,7 @@ for(i in 1:1000){
   plaspsvdef[[i]]<-plaspsv[[i]]%>%dplyr::select("PSV")
 }
 plaspsv1000=do.call(cbind, plaspsvdef)
-plaspsv1000df=plaspsv1000%>%cbind(Pid)%>%merge(plasgeo,by="X")
-View(plaspsv1000df)
+plaspsv1000df=plaspsv1000%>%cbind(plasgeoR[c("x","y")])
 write.csv(plaspsv1000df,"plaspsv1000.csv")#Save 1000 PSV for Plasmodium assemblages
 haenewdata=foreach::foreach(i = 1:1000)%dopar%{#Haemoproteus data
   picante::match.phylo.comm(haetrr[[i]],haepresabF)                          
@@ -70,10 +71,10 @@ for(i in 1:1000){
   haepsvdef[[i]]<-haepsv[[i]]%>%dplyr::select("PSV")
 }
 haepsv1000=do.call(cbind, haepsvdef)
-haepsv1000df=haepsv1000%>%cbind(Hid)%>%merge(haegeo,by="X")
-View(haepsv1000df)
+haepsv1000df=haepsv1000%>%cbind(haegeoR[c("x","y")])
 write.csv(haepsv1000df,"haepsv1000.csv")#Save PSV for Haemoproteus assemblages
-leunewdata=foreach::foreach(i = 1:10)%dopar%{#Leucocytozoon data
+
+leunewdata=foreach::foreach(i = 1:1000)%dopar%{#Leucocytozoon data
   picante::match.phylo.comm(leutrr[[i]],leupresabF)                          
 }
 leupsv=foreach::foreach(i = 1:1000)%dopar%{ #PSV for Leucocytozoon
@@ -84,15 +85,14 @@ for(i in 1:1000){
   leupsvdef[[i]]<-leupsv[[i]]%>%dplyr::select("PSV")
 }
 leupsv1000=do.call(cbind, leupsvdef)
-leupsv1000df=leupsv1000%>%cbind(Lid)%>%merge(leugeo,by="X")
-View(leupsv1000df)
+leupsv1000df=leupsv1000%>%cbind(leugeoR[c("x","y")])
 write.csv(leupsv1000df,"leupsv1000.csv")#Save PSV for Leucocytozoon assemblages
 
 parallel::stopCluster(my.cluster)
 
 ##PD
 my.cluster <- parallel::makeCluster(
-  5, 
+  6, 
   type = "PSOCK"
 )
 
@@ -104,45 +104,39 @@ plaspddef=list()
 for(i in 1:1000){
   plaspddef[[i]]<-plaspd[[i]]%>%dplyr::select("PD")
 }
-plasrichness=plaspresabF[-1]%>%rowSums()%>%as.data.frame()%>%cbind(Pid)#SR for Plasmodium
-names(plasrichness)=c("plasrichness","X")
+plasrichness=plaspresabF[-c(1:4)]%>%rowSums()%>%as.data.frame()#SR for Plasmodium
+names(plasrichness)=c("plasrichness")
 plaspd1000=do.call(cbind, plaspddef)
-plaspd1000=plaspd1000%>%cbind(Pid)%>%na.omit()
-dim(plaspd1000)
 dataplas=list()
 plasrpd=list()
-for (i in 1:10){
-  dataplas[[i]]=plaspd1000[c(i,11)]%>%merge(plasrichness,by="X")
+for (i in 1:1000){
+  dataplas[[i]]=plaspd1000[c(i)]%>%cbind(plasrichness,(plasgeoR[c(3,4)]))%>%na.omit()
   plasrpd[[i]]=as.data.frame(resid(gam(PD~s(plasrichness),data=dataplas[[i]]))) #RPD for Plasmodium as residuals of gam regression of pd vs sr
 }
 plasrpd1000=do.call(cbind, plasrpd)
-plasrpd1000df=plasrpd1000%>%cbind(plaspd1000[11])%>%merge(plasgeo,by="X")
-names(plasrpd1000df)=c("X",rep("rPD",10),"poligons","x","y")
-View(plasrpd1000df)
+plasrpd1000df=plasrpd1000%>%cbind(dataplas[[1]][c(3,4)])
+names(plasrpd1000df)=c(rep("rPD",1000),"x","y")
 write.csv(plasrpd1000df,"plasRPD1000.csv")#Save RPD for Plasmodium assemblages
-haepd=foreach::foreach(i = 1:10)%dopar%{
+haepd=foreach::foreach(i = 1:1000)%dopar%{
   picante::pd(haenewdata[[i]]$comm,haenewdata[[i]]$phy,include.root=F )#PD for Haemoproteus
 }
 haepddef=list()
 for(i in 1:1000){
   haepddef[[i]]<-haepd[[i]]%>%dplyr::select("PD")
 }
-haerichness=haepresabF[-1]%>%rowSums()%>%as.data.frame()%>%cbind(Hid)
-names(haerichness)=c("haerichness","X")
+haerichness=haepresabF[-c(1:4)]%>%rowSums()%>%as.data.frame()#SR Haemoproteus
+names(haerichness)=c("haerichness")
 haepd1000=do.call(cbind, haepddef)
-haepd1000=haepd1000%>%cbind(Hid)%>%na.omit()
-dim(haepd1000)
 ##rpd
 datahae=list()
 haerpd=list()
-for (i in 1:10){
-  datahae[[i]]=haepd1000[c(i,11)]%>%merge(haerichness,by="X")
+for (i in 1:1000){
+  datahae[[i]]=haepd1000[c(i)]%>%cbind(haerichness,(haegeoR[c(3,4)]))%>%na.omit()
   haerpd[[i]]=as.data.frame(resid(gam(PD~s(haerichness),data=datahae[[i]])))#RPD for Haemoproteus as residuals of gam regression of pd vs sr
 }
 haerpd1000=do.call(cbind, haerpd)
-haepsv1000df=haerpd1000%>%cbind(haepd1000[11])%>%merge(haegeo,by="X")
-names(haepsv1000df)=c("X",rep("rPD",10),"poligons","x","y")
-View(haepsv1000df)
+haepsv1000df=haerpd1000%>%cbind(datahae[[1]][c(3,4)])
+names(haepsv1000df)=c(rep("rPD",1000),"x","y")
 write.csv(haepsv1000df,"haerPD1000.csv")#Save RPD for Haemoproteus assemblages
 leupd=foreach::foreach(i = 1:10)%dopar%{
   picante::pd(leunewdata[[i]]$comm,leunewdata[[i]]$phy,include.root=F )###MEASURE PSV FOR EACH ASSEMBLAG
@@ -157,6 +151,16 @@ leupd1000=do.call(cbind, leupddef)
 leupd1000=leupd1000%>%cbind(Lid)%>%na.omit()
 dim(leupd1000)
 ##rpd
+
+
+##FINISH
+
+
+
+
+
+
+
 dataleu=list()
 leurpd=list()
 for (i in 1:1000){
