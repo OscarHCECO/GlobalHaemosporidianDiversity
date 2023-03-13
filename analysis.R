c5220ecfa7980d7abc8e5c6520217d638a966eb9
OscarHCECO <oscar.hernandez@posgrado.ecologia.edu.mx>
2023-03-13 00:20:17 -0600

    varimp

--- analysis.R
+++ analysis.R
@@ -4,113 +4,219 @@ library(geoGAM)
 library(mgcv)
 library(caret)
 library(doParallel)
-haerpd=read.csv("haerPD1000.csv",row.names = 1)%>%
+# Varimp analysis
+control <- trainControl(method = 'repeatedcv',# Train model to identify best predictors
+                        number = 1000,
+                        repeats = 1000,
+                        search = 'grid')
+#Haemoproteus ####
+haepredictors=read.csv("haepredictors.csv")#load predictors dataset 
+#Haemoproteus RPD
+haerpd=read.csv("haerPD1000.csv",row.names = 1)%>%# Load measures of Haemoproteus RPD based on 1000 phylogenetic trees
   purrr::set_names(c(rep("RPD",1000),"x","y"))
-haepredictors=read.csv("haepredictors.csv")
-#1000dataframes for each tree measures
-#rPD
 haerpdxy<-list()
 haerpddf<-list()
 for (i in 1:1000){
   haerpdxy[[i]]<-haerpd[c(i,1001,1002)]
-  haerpddf[[i]]<-haerpdxy[[i]]%>%merge(haepredictors,by=c("x","y"))
+  haerpddf[[i]]<-haerpdxy[[i]]%>%merge(haepredictors,by=c("x","y"))%>%na.omit()
 }
-###TRAIN MODELS TO IDENTIFY IMPORTANT PREDICTORS
-control <- trainControl(method = 'repeatedcv',
-                        number = 1000,
-                        repeats = 1000,
-                        search = 'grid')
 my.cluster <- parallel::makeCluster(
   8, 
-  type = "PSOCK"
-)
+  type = "PSOCK")
 doParallel::registerDoParallel(cl = my.cluster)
-f=foreach::foreach(i = 1:250)%dopar%{
-  caret::train(psv~.,modelType="gam",metric="Rsquared",data=haerpd[[i]],
+haerpdvarimp=foreach::foreach(i = 1:1000)%dopar%{
+  caret::train(RPD~.,modelType="gam",metric="Rsquared",data=haerpddf[[i]],
                tuneLength  = 2,control=control,family = "gaussian")                          
 }
-
-varimp=list()
-imp1<-list()
-for(i in 1:250){
-  varimp[[i]]<-caret::varImp(f[[i]],scale=T)
+for(i in 1:1000){haerpdvarimpsc[[i]]<-caret::varImp(haerpdvarimp[[i]],scale=T)
+haerpdvarimpsc=list()
+haerpdvarimpscore<-list()
 }
-for(i in 1:250){
-imp1[[i]] <- data.frame(overall = varimp[[i]]$importance$Overall,
-                   names   = rownames(varimp[[i]]$importance))
-
-imp1[[i]]=imp1[[i]][order(imp1[[i]]$names,decreasing=T),]
+for(i in 1:1000){
+  haerpdvarimpscore[[i]] <- data.frame(overall = haerpdvarimpsc[[i]]$importance$Overall,
+                                       names   = rownames(haerpdvarimpsc[[i]]$importance))
+  haerpdvarimpscore[[i]]=haerpdvarimpscore[[i]][order(haerpdvarimpscore[[i]]$names,decreasing=T),]
 }
-
-varim1=imp1%>%plyr::ldply(rbind)
-
-haegeneralism=write.csv(varim1,"leupsv1imp.csv")
-
+haerpdvarimpscoredf=haerpdvarimpscore%>%plyr::ldply(rbind)
+write.csv(haerpdvarimpscoredf,"haevarimprpd1000.csv")
+#Haemoproteus PSV
+haepsv=read.csv("haepsv1000.csv",row.names = 1)%>%
+  purrr::set_names(c(rep("psv",1000),"x","y"))
+haepsvxy<-list()
+haepsvdf<-list()
+for (i in 1:1000){
+  haepsvxy[[i]]<-haepsv[c(i,1001,1002)]
+  haepsvdf[[i]]<-haepsvxy[[i]]%>%merge(haepredictors,by=c("x","y"))%>%na.omit()
+}
+my.cluster <- parallel::makeCluster(
+  8, 
+  type = "PSOCK")
 doParallel::registerDoParallel(cl = my.cluster)
-
-f=foreach::foreach(i = 251:500)%dopar%{
-  caret::train(psv~.,modelType="gam",metric="Rsquared",data=haerpd[[i]],
+haepsvvarimp=foreach::foreach(i = 1:1000)%dopar%{
+  caret::train(psv~.,modelType="gam",metric="Rsquared",data=haepsvdf[[i]],
                tuneLength  = 2,control=control,family = "gaussian")                          
 }
-
-varimp=list()
-imp1<-list()
-for(i in 1:250){
-    varimp[[i]]<-caret::varImp(f[[i]],scale=T)
-    imp1[[i]] <- data.frame(overall = varimp[[i]]$importance$Overall,names   = rownames(varimp[[i]]$importance))
-    imp1[[i]]=imp1[[i]][order(imp1[[i]]$names,decreasing=T),]
+haepsvvarimpsc=list()
+haepsvvarimpscore<-list()
+for(i in 1:1000){
+  haepsvvarimpsc[[i]]<-caret::varImp(haepsvvarimp[[i]],scale=T)
 }
-library(dplyr)
-varim1=imp1%>%plyr::ldply(rbind)
-
-haegeneralism1=write.csv(varim1,"leupsv2imp.csv")
+for(i in 1:1000){
+  haepsvvarimpscore[[i]] <- data.frame(overall = haepsvvarimpsc[[i]]$importance$Overall,
+                                       names   = rownames(haepsvvarimpsc[[i]]$importance))
+  haepsvvarimpscore[[i]]=haepsvvarimpscore[[i]][order(haepsvvarimpscore[[i]]$names,decreasing=T),]
+}
+haepsvvarimpscoredf=haepsvvarimpscore%>%plyr::ldply(rbind)
+write.csv(haepsvvarimpscoredf,"haevarimppsv1000.csv")
+##############################################################################################################
+# Plasmodium ####
+plaspredictors=read.csv("plaspredictors.csv")
+# Plasmodium RPD
+plasrpd=read.csv("plasRPD1000.csv",row.names = 1)%>%# Load measures of Plasmodium RPD based on 1000 phylogenetic trees
+  purrr::set_names(c(rep("RPD",1000),"x","y"))
+plasrpdxy<-list()
+plasrpddf<-list()
+for (i in 1:1000){
+  plasrpdxy[[i]]<-plasrpd[c(i,1001,1002)]
+  plasrpddf[[i]]<-plasrpdxy[[i]]%>%merge(plaspredictors,by=c("x","y"))%>%na.omit()
+}
+my.cluster <- parallel::makeCluster(
+  8, 
+  type = "PSOCK")
+doParallel::registerDoParallel(cl = my.cluster)
+plasrpdvarimp=foreach::foreach(i = 1:1000)%dopar%{
+  caret::train(RPD~.,modelType="gam",metric="Rsquared",data=plasrpddf[[i]],
+               tuneLength  = 2,control=control,family = "gaussian")                          
+}
+plasrpdvarimpsc=list()
+plasrpdvarimpscore<-list()
+for(i in 1:1000){
+  plasrpdvarimpsc[[i]]<-caret::varImp(plasrpdvarimp[[i]],scale=T)
+}
+for(i in 1:1000){
+  plasrpdvarimpscore[[i]] <- data.frame(overall = plasrpdvarimpsc[[i]]$importance$Overall,
+                                        names   = rownames(plasrpdvarimpsc[[i]]$importance))
+  plasrpdvarimpscore[[i]]=plasrpdvarimpscore[[i]][order(plasrpdvarimpscore[[i]]$names,decreasing=T),]
+}
+plasrpdvarimpscoredf=plasrpdvarimpscore%>%plyr::ldply(rbind)
+write.csv(plasrpdvarimpscoredf,"plasvarimprpd1000.csv")
+# Plasmodium PSV
+plaspsv=read.csv("plaspsv1000.csv",row.names = 1)%>%
+  purrr::set_names(c(rep("psv",1000),"x","y"))
+plaspredictors=read.csv("plaspredictors.csv")
+plaspsvxy<-list()
+plaspsvdf<-list()
+for (i in 1:1000){
+  plaspsvxy[[i]]<-plaspsv[c(i,1001,1002)]
+  plaspsvdf[[i]]<-plaspsvxy[[i]]%>%merge(plaspredictors,by=c("x","y"))%>%na.omit()
+}
+my.cluster <- parallel::makeCluster(
+  8, 
+  type = "PSOCK")
+doParallel::registerDoParallel(cl = my.cluster)
+plaspsvvarimp=foreach::foreach(i = 1:250)%dopar%{
+  caret::train(psv~.,modelType="gam",metric="Rsquared",data=plaspsvdf[[i]],
+               tuneLength  = 2,control=control,family = "gaussian")                          
+}
+plaspsvvarimpsc=list()
+plaspsvvarimpscore<-list()
+for(i in 1:250){
+  plaspsvvarimpsc[[i]]<-caret::varImp(plaspsvvarimp[[i]],scale=T)
+}
+for(i in 1:250){
+  plaspsvvarimpscore[[i]] <- data.frame(overall = plaspsvvarimpsc[[i]]$importance$Overall,
+                                        names   = rownames(plaspsvvarimpsc[[i]]$importance))
+  plaspsvvarimpscore[[i]]=plaspsvvarimpscore[[i]][order(plaspsvvarimpscore[[i]]$names,decreasing=T),]
+}
+plaspsvvarimpscoredf=plaspsvvarimpscore%>%plyr::ldply(rbind)
+write.csv(plaspsvvarimpscoredf,"plasvarimppsv1000.csv")
+##############################################################################################################
+# Leucocytozoon ####
+leupredictors=read.csv("leupredictors.csv")
+# Leucocytozoon RPD
+leurpd=read.csv("leuRPD1000.csv",row.names = 1)%>%# Load measures of Plasmodium RPD based on 1000 phylogenetic trees
+  purrr::set_names(c(rep("RPD",1000),"x","y"))
+leurpdxy<-list()
+leurpddf<-list()
+for (i in 1:1000){
+  leurpdxy[[i]]<-leurpd[c(i,1001,1002)]
+  leurpddf[[i]]<-leurpdxy[[i]]%>%merge(leupredictors,by=c("x","y"))%>%na.omit()
+}
+my.cluster <- parallel::makeCluster(
+  8, 
+  type = "PSOCK")
+doParallel::registerDoParallel(cl = my.cluster)
+leurpdvarimp=foreach::foreach(i = 1:1000)%dopar%{
+  caret::train(RPD~.,modelType="gam",metric="Rsquared",data=leurpddf[[i]],
+               tuneLength  = 2,control=control,family = "gaussian")                          
+}
+leurpdvarimpsc=list()
+leurpdvarimpscore<-list()
+for(i in 1:1000){
+  leurpdvarimpsc[[i]]<-caret::varImp(leurpdvarimp[[i]],scale=T)
+}
+for(i in 1:1000){
+  leurpdvarimpscore[[i]] <- data.frame(overall = leurpdvarimpsc[[i]]$importance$Overall,
+                                       names   = rownames(leurpdvarimpsc[[i]]$importance))
+  leurpdvarimpscore[[i]]=leurpdvarimpscore[[i]][order(leurpdvarimpscore[[i]]$names,decreasing=T),]
+}
+leurpdvarimpscoredf=leurpdvarimpscore%>%plyr::ldply(rbind)
+write.csv(leurpdvarimpscoredf,"leuvarimprpd1000.csv")
+#Leucocytozoon psv
+leupredictors=read.csv("leupredictors.csv")
+leupsv=read.csv("leupsv1000.csv",row.names = 1)%>%
+  purrr::set_names(c(rep("psv",1000),"x","y"))
+#1000dataframes for each tree measures
+#psv
+leupsvxy<-list()
+leupsvdf<-list()
+for (i in 1:1000){
+  leupsvxy[[i]]<-leupsv[c(i,1001,1002)]
+  leupsvdf[[i]]<-leupsvxy[[i]]%>%merge(leupredictors,by=c("x","y"))%>%na.omit()
+}
+my.cluster <- parallel::makeCluster(
+  8, 
+  type = "PSOCK")
+doParallel::registerDoParallel(cl = my.cluster)
+leupsvvarimp=foreach::foreach(i = 1:1000)%dopar%{
+  caret::train(psv~.,modelType="gam",metric="Rsquared",data=leupsvdf[[i]],
+               tuneLength  = 2,control=control,family = "gaussian")                          
+}
+leupsvvarimpsc=list()
+leupsvvarimpscore<-list()
+for(i in 1:1000){
+  leupsvvarimpsc[[i]]<-caret::varImp(leupsvvarimp[[i]],scale=T)
+}
+for(i in 1:1000){
+  leupsvvarimpscore[[i]] <- data.frame(overall = leupsvvarimpsc[[i]]$importance$Overall,
+                                       names   = rownames(leupsvvarimpsc[[i]]$importance))
+  
+  leupsvvarimpscore[[i]]=leupsvvarimpscore[[i]][order(leupsvvarimpscore[[i]]$names,decreasing=T),]
+}
+leupsvvarimpscoredf=leupsvvarimpscore%>%plyr::ldply(rbind)
+write.csv(leupsvvarimpscoredf,"leuvarimppsv1000.csv")
+# Leucocytozoon richness
+leurichness=read.csv("haedependent.csv",row.names = 1)%>%select(c("SR","x","y"))%>%
+  merge(leupredictors,by=c("x","y"))%>%na.omit()
 
 my.cluster <- parallel::makeCluster(
-  10, 
-  type = "PSOCK"
-)
+  8, 
+  type = "PSOCK")
 doParallel::registerDoParallel(cl = my.cluster)
-f=foreach::foreach(i = 501:750)%dopar%{
-  caret::train(psv~.,modelType="gam",metric="Rsquared",data=haerpd[[i]],
+leupsvvarimp=foreach::foreach(i = 1:1000)%dopar%{
+  caret::train(psv~.,modelType="gam",metric="Rsquared",data=leupsvdf[[i]],
                tuneLength  = 2,control=control,family = "gaussian")                          
 }
-
-varimp=list()
-imp1<-list()
-for(i in 1:250){
-  varimp[[i]]<-caret::varImp(f[[i]],scale=T)
-  imp1[[i]] <- data.frame(overall = varimp[[i]]$importance$Overall,names   = rownames(varimp[[i]]$importance))
-  imp1[[i]]=imp1[[i]][order(imp1[[i]]$names,decreasing=T),]
+leupsvvarimpsc=list()
+leupsvvarimpscore<-list()
+for(i in 1:1000){
+  leupsvvarimpsc[[i]]<-caret::varImp(leupsvvarimp[[i]],scale=T)
 }
-varim1=imp1%>%plyr::ldply(rbind)
-
-haegeneralism1=write.csv(varim1,"leupsv3imp.csv")
-
-
-doParallel::registerDoParallel(cl = my.cluster)
-f=foreach::foreach(i = 751:1000)%dopar%{
-  caret::train(psv~.,modelType="gam",metric="Rsquared",data=haerpd[[i]],
-               tuneLength  = 2,control=control,family = "gaussian")                          
+for(i in 1:1000){
+  leupsvvarimpscore[[i]] <- data.frame(overall = leupsvvarimpsc[[i]]$importance$Overall,
+                                       names   = rownames(leupsvvarimpsc[[i]]$importance))
+  
+  leupsvvarimpscore[[i]]=leupsvvarimpscore[[i]][order(leupsvvarimpscore[[i]]$names,decreasing=T),]
 }
-
-varimp=list()
-imp1<-list()
-for(i in 1:250){
-  varimp[[i]]<-caret::varImp(f[[i]],scale=T)
-  imp1[[i]] <- data.frame(overall = varimp[[i]]$importance$Overall,names   = rownames(varimp[[i]]$importance))
-  imp1[[i]]=imp1[[i]][order(imp1[[i]]$names,decreasing=T),]
-}
-varim1=imp1%>%plyr::ldply(rbind)
-
-haegeneralism1=write.csv(varim1,"leupsv4imp.csv")
-
-
-
-
-haegeneralism=read.csv("leupsv2imp.csv")
-haegeneralism1=read.csv("leupsv3imp.csv")
-haegeneralism2=read.csv("leupsv4imp.csv")
-haegeneralism3=read.csv("leupsv1imp.csv")
-new=rbind(haegeneralism1,haegeneralism3,haegeneralism2,haegeneralism)
-write.csv(new,"varimpleupsvF.csv")
-1
+leupsvvarimpscoredf=leupsvvarimpscore%>%plyr::ldply(rbind)
+write.csv(leupsvvarimpscoredf,"leuvarimppsv1000.csv")
