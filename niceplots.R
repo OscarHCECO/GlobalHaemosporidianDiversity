2a67f108f7de88f0a89f95c0a1f268a06308e517
OscarHCECO <125691793+OscarHCECO@users.noreply.github.com>
2023-04-20 09:51:07 -0600

    Delete niceplots.R
    deleteed script

--- niceplots.R
+++
@@ -1,5 +0,0 @@
-finaluno=cowplot::plot_grid(A,B,C,e = "AUTO",nrow=1,ncol=3,rel_widths = c(1,1,1),rel_heights=c(1,1,1),align="hv")
-
-tiff("C:/Users/oskar/Downloads/figure2.tiff", units="px", width=1800*2, height=1476, res=300)
-finaluno
-dev.off()
